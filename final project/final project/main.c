8bda3298d398ac77d528a292422cb64bab9d9718
Joe Maloney <maloney.jg2@gmail.com>
2025-04-24 10:48:09 -0400

    init

---
+++ final project/final project/main.c
@@ -0,0 +1,18 @@
+/*
+ * final project.c
+ *
+ * Created: 4/24/2025 10:46:21 AM
+ * Author : malon
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
